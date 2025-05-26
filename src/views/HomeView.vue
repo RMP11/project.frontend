a35c3ce92c23f0e612393b118ef2f70a0e342f89
Rider Mamani Paco <rider.mamani@ucb.edu.bo>
2025-05-26 19:18:05 -0400

    refactor: titulo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="flex flex-col justify-center gap-3 items-center w-full h-full">
     <h1 class="text-7xl font-bold">Bienvenidos</h1>
-    <h2 class="text-5xl font-medium text-center">Sistema de Venta Auto Partes</h2>
+    <h2 class="text-5xl font-medium text-center">Sistema de Venta de Auto Partes</h2>
   </div>
 </template>
